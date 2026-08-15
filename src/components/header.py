68d9dfd0dbef2370c3d867128081c094e6af6bc9
Aniket Kharose <aniketk8010@gmailcom>
2026-08-15 21:50:39 +0530

    edit buttons

--- src/components/header.py
+++ src/components/header.py
@@ -3,4 +3,11 @@ import streamlit as st
 
 
 def header_home():
-    st.header("Snap Class")
+    logo_url ="https://i.ibb.co/YTYGn5qV/logo.png"
+    
+    st.markdown(f"""
+        <div>
+            <img src='{logo_url}'style=height:100px />        
+            <h1 style=text_align:centerc>    </h1>
+            </div>    
+    """, unsafe_allow_html=True)
--- src/screen/home_screen.py
+++ src/screen/home_screen.py
@@ -1,10 +1,16 @@
 import streamlit as st 
-import src.componencts.header as home_header
+from src.components.header import header_home
+from src.ui.base_layout import style_base_layout ,style_background_home
+
+
 def home_screen():    
     
-    
-    
     header_home()
+    
+    style_background_home()
+    
+    style_base_layout()
+    
     col1, col2 = st.columns(2)
     
     
